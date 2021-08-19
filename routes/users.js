f1ca66f807a806a1330574f97729568a0e41c955
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-19 09:33:40 -0500

    user apis done

--- routes/users.js
+++ routes/users.js
@@ -35,11 +35,81 @@ router.put("/:id", verify, async (req, res) => {
 
 // DELETE
 
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can delete only your account!");
+    }
+  });
+
 // GET
+
+router.get("/find/:id", async (req, res) => {
+
+      try {
+
+        const user = await User.findById(req.params.id);
+        const { password, ...info } = user._doc
+        res.status(200).json(info);
+
+      } catch (err) 
+      {
+        res.status(500).json(err);
+
+      }
+  });
 
 // GET ALL
 
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+      try {
+        const users = query
+          ? await User.find().sort({ _id: -1 }).limit(5)
+          : await User.find();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  );
+
 // GET USER STATS
+
+router.get('/stats', async (req, res) => {
+
+    const today = new Date()
+    const latYear = today.setFullYear(today.setFullYear() - 1)
+
+
+    try {
+
+        const data = await User.aggregate([
+            {
+                $project: {
+                    month: {$month: "$createdAt"}
+                }
+            },{
+                $group: {
+                    _id: "$month",
+                    total: {$sum:1}
+                }
+            }
+        ])
+
+        res.status(200).json(data)
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+
+})
 
 
 module.exports = router
