ccc1dd1946187782751ada49553930ebf55b16e0
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-19 20:18:43 -0500

    changes

--- routes/users.js
+++ routes/users.js
@@ -65,51 +65,6 @@ router.get("/find/:id", async (req, res) => {
       }
   });
 
-// GET ALL
-
-router.get("/", verify, async (req, res) => {
-    const query = req.query.new;
-      try {
-        const users = query
-          ? await User.find().sort({ _id: -1 }).limit(5)
-          : await User.find();
-        res.status(200).json(users);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    }
-  );
-
-// GET USER STATS
-
-router.get('/stats', async (req, res) => {
-
-    const today = new Date()
-    const latYear = today.setFullYear(today.setFullYear() - 1)
-
-
-    try {
-
-        const data = await User.aggregate([
-            {
-                $project: {
-                    month: {$month: "$createdAt"}
-                }
-            },{
-                $group: {
-                    _id: "$month",
-                    total: {$sum:1}
-                }
-            }
-        ])
-
-        res.status(200).json(data)
-
-    } catch(error) {
-        res.status(500).json(error)
-    }
-
-})
 
 
 module.exports = router
